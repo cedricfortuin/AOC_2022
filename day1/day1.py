d3588d157d3ad8508c26a60fcceff4558f23b10c
cedricfortuin <cedric.laurent.fortuin@gmail.com>
2022-12-01 11:11:09 +0100

    add day 1.2

--- day1/day1.py
+++ day1/day1.py
@@ -1,14 +1,17 @@
-def day1(file):
-    print("Day 1: {}".format(max(file)))
+def day1(yeet):
+    print("Day 1.1: {}".format(max(yeet)))
 
 
+def day2(yeet):
+    print("Day 1.2: {}".format(sum(sorted(yeet, reverse=True)[:3])))
 
 
 def main():
-    with open('test.txt') as file:
+    with open('input.txt') as file:
         input_file = file.read().split('\n\n')
         total = [sum([int(i) for i in k.split('\n')]) for k in input_file]
         day1(total)
+        day2(total)
 
 if __name__ == '__main__':
     main()
